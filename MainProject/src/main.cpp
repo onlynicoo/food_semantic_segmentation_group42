55c7a06be88e9179052383ddb802d3700c1f00b1
Daniele Moschetta <daniele.moschetta@studenti.unipd.it>
2023-07-05 10:49:15 +0200

    added arg check

--- MainProject/src/main.cpp
+++ MainProject/src/main.cpp
@@ -1,22 +1,20 @@
 #include <opencv2/opencv.hpp>
 #include "../include/Tray.h"
 
-using namespace cv;
 
-Mat src;
-const char* window_plates = "src";
+int main( int argc, char** argv ) {
 
-int main( int argc, char** argv )
-{
-    // read the image
+    // Read arguments
+    if (argc < 3) {
+        std::cout << "You have to pass <initial tray image path> <final tray image path> as arguments." << std::endl;
+        return 1;
+    }
     std::string before = argv[1];
     std::string after = argv[2];
     
     Tray my_tray = Tray(before, after);
     
-    my_tray.PrintInfo(); 
-
-    //cv::waitKey(0);
-
+    my_tray.PrintInfo();
+    
     return 0;
 }
