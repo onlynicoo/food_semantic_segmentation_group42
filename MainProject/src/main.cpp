902435bfbb275ba6b4927cc8d9c504a9238ecefe
Daniele Moschetta <daniele.moschetta@studenti.unipd.it>
2023-07-11 12:34:52 +0200

    added option to iterate over all the trays

--- MainProject/src/main.cpp
+++ MainProject/src/main.cpp
@@ -1,20 +1,24 @@
 #include <opencv2/opencv.hpp>
 #include "../include/Tray.h"
 
-
 int main( int argc, char** argv ) {
 
     // Read arguments
     if (argc < 3) {
-        std::cout << "You have to pass <initial tray image path> <final tray image path> as arguments." << std::endl;
-        return 1;
+        for (int left = 1; left <= 3; left++) {
+            for (int tray = 1; tray <= 8; tray++) {
+                std::cout << "Tray " + std::to_string(tray) << " Leftover " << std::to_string(left) << std::endl;
+                Tray my_tray = Tray("../../../data/Food_leftover_dataset/tray" + std::to_string(tray) + "/food_image.jpg",
+                                    "../../../data/Food_leftover_dataset/tray" + std::to_string(tray) + "/leftover" + std::to_string(left) + ".jpg");
+                my_tray.PrintInfo();
+                std::cout << std::endl;
+            }
+        }
+    } else {
+        std::string before = argv[1];
+        std::string after = argv[2];
+        Tray my_tray = Tray(before, after);
     }
-    std::string before = argv[1];
-    std::string after = argv[2];
-    
-    Tray my_tray = Tray(before, after);
-    
-    my_tray.PrintInfo();
-    
+
     return 0;
 }
